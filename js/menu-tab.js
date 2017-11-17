98e205b8b13abe010f1e190e944abe84745ba8c1
joylty94 <wowmen1757@gmail.com>
2017-11-17 17:03:30 +0900

    로그인 폼 관련 영역 일부 마크업 완성

--- js/menu-tab.js
+++ js/menu-tab.js
@@ -1,15 +1,20 @@
 $(document).ready(function() {
   var menu = $('.main-menu > li');
-  var subMenu = $('.sub-menu');
-  var last = $('.sub-menu li:last-child a')
 
   menu.hover(function() {
-    $(this).find(subMenu).toggleClass('menu-act');
+    $(this).find('.sub-menu').toggleClass('menu-act')
   });
-  menu.focus(function() {
-    $(this).find(subMenu).addClass('menu-act');
-  });
-  list.focusout(function() {
-    $(this).parents('.sub-menu').removeClass('menu-act');
-  })
+  // $('.main-menu>li').on('focusin', function() {
+  //   $(this).find('.sub-menu').addClass('menu-act')
+  // })
+  // $('.sub-menu li:last-child a').on('focusout', function() {
+  //   $(this).parents('.sub-menu').removeClass('menu-act')
+  // })
+  menu.focusin(function() {
+      $(this).siblings().find('.sub-menu').removeClass('menu-act');
+      $(this).find('.sub-menu').addClass('menu-act');
+    })
+    // $('.box').click(function() {
+    //   $(this).toggleClass('box-act');
+    // })
 });
